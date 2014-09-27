aacb21ff5174397535d9cedab4dcd32ea49c4699
Priyandiono <ifdion@gmail.com>
2014-09-27 10:47:24 +0700

    Use original user notification message

--- cell-email.php
+++ cell-email.php
@@ -91,7 +91,17 @@ if( ! function_exists('wp_new_user_notification') ) {
 
 		wp_mail($user_email, $email_subject, $message);
 
-		wp_mail(get_bloginfo('admin_email'), $email_subject, $message);
+		// notification to admin
+
+	    // The blogname option is escaped with esc_html on the way into the database in sanitize_option
+	    // we want to reverse this for the plain text arena of emails.
+	    $blogname = wp_specialchars_decode(get_option('blogname'), ENT_QUOTES);
+
+	    $message = sprintf(__('New user registration on your site %s:'), $blogname) . "\r\n\r\n";
+	    $message .= sprintf(__('Username: %s'), $user->user_login) . "\r\n\r\n";
+	    $message .= sprintf(__('E-mail: %s'), $user->user_email) . "\r\n";
+
+	    wp_mail(get_option('admin_email'), sprintf(__('[%s] New User Registration'), $blogname), $message);
 
 	}
 
